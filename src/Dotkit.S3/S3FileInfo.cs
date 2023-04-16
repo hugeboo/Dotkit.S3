349f06615499715fe68271d06452caea615c4664
hugeboo <sesv@yandex.ru>
2023-04-16 13:21:30 +0300

    Enumerate directories

--- src/Dotkit.S3/S3FileInfo.cs
+++ src/Dotkit.S3/S3FileInfo.cs
@@ -1,12 +1,169 @@
-﻿using System;
+﻿using Amazon.Runtime.Internal;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Dotkit.S3
 {
-    public sealed class S3FileInfo //: IS3FileSystemInfo
+    /// <summary>
+    /// Операции с файлом в рамках бакета S3 хранилища.
+    /// <inheritdoc/>
+    /// </summary>
+    public sealed class S3FileInfo : IS3FileSystemInfo
     {
+        private readonly string _bucketName;
+        private readonly IAmazonS3 _s3Client;
+        private readonly string _key;
+        private readonly S3DirectoryInfo _directory;
+
+        public bool Exists {get; private set;}
+
+        public string Extension
+        {
+            get
+            {
+                int num = Name.LastIndexOf('.');
+                if (num == -1 || Name.Length <= num + 1)
+                {
+                    return string.Empty;
+                }
+
+                return Name.Substring(num + 1);
+            }
+        }
+
+        public string FullName => $"{_bucketName}:\\{_key}";
+
+        public DateTime LastModifiedTime { get; private set; }
+
+        public string Name
+        {
+            get
+            {
+                int num = _key.LastIndexOf('\\');
+                return _key.Substring(num + 1);
+            }
+        }
+
+        public FileSystemType Type => FileSystemType.File;
+
+        /// <summary>
+        /// Returns the parent S3DirectoryInfo
+        /// </summary>
+        public S3DirectoryInfo Directory => Directory;
+
+        /// <summary>
+        /// The full name of parent directory
+        /// </summary>
+        public string DirectoryName => Directory.FullName;
+
+        /// <summary>
+        /// Returns the content length of the file
+        /// </summary>
+        public long Length { get; private set; }
+
+        /// <summary>
+        /// Конструктор класса файла
+        /// </summary>
+        /// <param name="s3Client">Клиент для связи с S3 хранилищем</param>
+        /// <param name="bucket">Имя бакета</param>
+        /// <param name="key">Путь к директории</param>
+        internal S3FileInfo(IAmazonS3 s3Client, string bucket, string key)
+        {
+            if (string.IsNullOrEmpty(key) || string.Equals(key, "\\"))
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (key.EndsWith("\\", StringComparison.Ordinal))
+            {
+                throw new ArgumentException("key is a directory name");
+            }
+
+            _s3Client = s3Client;
+            _bucketName = bucket;
+            _key = key;
+
+            string dirKey = null!;
+            int num = key.LastIndexOf('\\');
+            if (num >= 0)
+            {
+                dirKey = key.Substring(0, num);
+            }
+            else
+            {
+                new ArgumentException("key contains bad directory");
+            }
+            _directory = new S3DirectoryInfo(s3Client, bucket, dirKey);
+        }
+
+        /// <summary>
+        /// Актуалзировать информацию (Exists, LastModifiedTime, Length) из S3 хранилища
+        /// </summary>
+        internal async Task InitFromRemoteAsync()
+        {
+            try
+            {
+                var request = new ListObjectsV2Request
+                {
+                    BucketName = _bucketName,
+                    Prefix = S3Helper.EncodeKey(_key),
+                    MaxKeys = 1
+                };
+                var response = await _s3Client.ListObjectsV2Async(request);
+                if (response.HttpStatusCode == System.Net.HttpStatusCode.OK && response.S3Objects.Count == 1)
+                {
+                    Exists = true;
+                    LastModifiedTime = response.S3Objects[0].LastModified;
+                }
+                else
+                {
+                    Exists = false;
+                    LastModifiedTime = DateTime.MinValue;
+                }
+
+                Length = 0L;
+                if (Exists)
+                {
+                    var getObjectMetadataRequest = new GetObjectMetadataRequest
+                    {
+                        BucketName = _bucketName,
+                        Key = S3Helper.EncodeKey(_key)
+                    };
+                    var metaResponse = await _s3Client.GetObjectMetadataAsync(getObjectMetadataRequest);
+                    Length = metaResponse.ContentLength;
+                }
+            }
+            catch (AmazonS3Exception ex)
+            {
+                if (string.Equals(ex.ErrorCode, "NotFound"))
+                {
+                    Exists = false;
+                    LastModifiedTime = DateTime.MinValue;
+                    Length = 0L;
+                }
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Актуалзировать информацию (Exists, LastModifiedTime, Length) из объекта S3 хранилища
+        /// </summary>
+        internal void InitFromRemoteObject(S3Object obj)
+        {
+            LastModifiedTime = obj.LastModified;
+            Length = obj.Size;
+        }
+
+        public Task DeleteAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- tests/Dotkit.S3.IntegrationTests/S3DirectoryTests.cs
+++ tests/Dotkit.S3.IntegrationTests/S3DirectoryTests.cs
@@ -5,7 +5,7 @@ namespace Dotkit.S3.IntegrationTests
     public class S3DirectoryTests
     {
         [Fact]
-        public void CreateDirectory_Success()
+        public void CreateDirectory()
         {
             var config = new S3Configuration();
             var service = config.CreateService();
@@ -23,7 +23,7 @@ namespace Dotkit.S3.IntegrationTests
         }
 
         [Fact]
-        public void RecursiveDeleteDirectory_Success()
+        public void RecursiveDeleteDirectory()
         {
             var config = new S3Configuration();
             var service = config.CreateService();
@@ -41,6 +41,71 @@ namespace Dotkit.S3.IntegrationTests
             
             di.DeleteAsync(true).Wait();
             Assert.False(di.Exists);
+        }
+
+        // Тестовая структура директорий:
+        //
+        // EnumerateTests
+        // |
+        // +-Level11
+        // | |
+        // | +-Level21
+        // | +-Level22
+        // |
+        // +-Level12
+        //   |
+        //   +-Level23
+        //     |
+        //     +-Level31
+        //     +-Level32
+
+        [Fact]
+        public void EnumerateDirectories()
+        {
+            var service = CreateEnumerateTestDirs();
+
+            var root = service.GetDirectoryInfoAsync("EnumerateTests").Result;
+
+            // 1
+            var lst1 = root.GetDirectories().Result;
+            Assert.Equal(2, lst1.Count);
+
+            // 2
+            var level12 = root.GetSubDirectoryInfoAsync("Level12").Result;
+            var lst2 = level12.GetDirectories().Result;
+            Assert.Single(lst2);
+
+            // 3
+            var level23 = service.GetDirectoryInfoAsync("EnumerateTests\\Level12\\Level23").Result;
+            var lst3 = level23.GetDirectories().Result;
+            Assert.Equal(2, lst3.Count);
+
+            // 3 - Check delete
+            level23.DeleteAsync(true).Wait();
+            lst3 = level23.GetDirectories().Result;
+            Assert.Empty(lst3);
+
+            root.DeleteAsync(true).Wait();
+            Assert.False(root.Exists);
+        }
+
+        private static IS3Service CreateEnumerateTestDirs()
+        {
+            var config = new S3Configuration();
+            var service = config.CreateService();
+
+            var root = service.GetDirectoryInfoAsync("EnumerateTests").Result.CreateAsync().Result;
+
+            var level11 = root.GetSubDirectoryInfoAsync("Level11").Result.CreateAsync().Result;
+            var level21 = level11.GetSubDirectoryInfoAsync("Level21").Result.CreateAsync().Result;
+            var level22 = level11.GetSubDirectoryInfoAsync("Level22").Result.CreateAsync().Result;
+
+            var level12 = root.GetSubDirectoryInfoAsync("Level12").Result.CreateAsync().Result;
+            var level23 = level12.GetSubDirectoryInfoAsync("Level23").Result.CreateAsync().Result;
+            var level31 = level23.GetSubDirectoryInfoAsync("Level31").Result.CreateAsync().Result;
+            var level32 = level23.GetSubDirectoryInfoAsync("Level32").Result.CreateAsync().Result;
+
+            return service;
         }
     }
 }
